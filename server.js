c517336f9df2f49d05a0dc5bb97326f1902ddd34
Prem Prasad <premrinkinoniya@gmail.com>
2025-12-28 18:53:21 +0530

    Updated Backened SetUp

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
  const app=require('./src/app');
+const connectDb=require('./src/config/db');
+
+connectDb();
 
  const PORT=process.env.PORT||1000;
  app.listen(PORT,()=>{
