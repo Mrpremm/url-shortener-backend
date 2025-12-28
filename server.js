f27c4f7d338e26be4ae11d0b315ccb1742c3a147
Prem Prasad <premrinkinoniya@gmail.com>
2025-12-28 18:44:23 +0530

    Updated Backened SetUp

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+ const app=require('./src/app');
+
+ const PORT=process.env.PORT||1000;
+ app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+ })
